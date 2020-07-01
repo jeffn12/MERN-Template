40c8e313a46d693982fca29fce0f956a9937b564
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-07-01 14:57:14 -0400

    Add GET functionality for users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,17 @@
 var express = require('express');
-var userModel = require('../models/user.model');
+var User = require('../models/user.model');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
+	User.find({}, (err, users) => {
+		if (err) {
+			console.error(err);
+			res.send('Error retrieving users');
+		} else {
+			res.send(users);
+		}
+	});
 });
 
 module.exports = router;
